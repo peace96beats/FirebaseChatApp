5d59a77d691ae3d94bfd3a801558c06e2d2dbcc7
atsushi <peace96beats@gmail.com>
2018-06-22 01:45:56 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const config = {
+    apiKey: "AIzaSyCxRr99zcPSgjrm0_ciZOL3J_w0qTwynTs",
+    authDomain: "myfirebasechatapp-e5cca.firebaseapp.com",
+    databaseURL: "https://myfirebasechatapp-e5cca.firebaseio.com",
+    projectId: "myfirebasechatapp-e5cca",
+    storageBucket: "myfirebasechatapp-e5cca.appspot.com",
+    messagingSenderId: "1045285314009"
+  };
+  firebase.initializeApp(config);
