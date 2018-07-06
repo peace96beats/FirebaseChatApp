825735560922c21a50f10a60ebf66f268d45f945
atsushi <peace96beats@gmail.com>
2018-07-06 20:30:18 +0900

    #9まで完了

--- js/main.js
+++ js/main.js
@@ -1,11 +1,60 @@
 'use strict'
 
 const config = {
-    apiKey: "AIzaSyCxRr99zcPSgjrm0_ciZOL3J_w0qTwynTs",
-    authDomain: "myfirebasechatapp-e5cca.firebaseapp.com",
-    databaseURL: "https://myfirebasechatapp-e5cca.firebaseio.com",
-    projectId: "myfirebasechatapp-e5cca",
-    storageBucket: "myfirebasechatapp-e5cca.appspot.com",
-    messagingSenderId: "1045285314009"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyDnPsMPeOmNGQkWfR3rg4homLR9IUtmMZE",
+  authDomain: "myfirebasechatapp-8a7de.firebaseapp.com",
+  databaseURL: "https://myfirebasechatapp-8a7de.firebaseio.com",
+  projectId: "myfirebasechatapp-8a7de",
+  storageBucket: "myfirebasechatapp-8a7de.appspot.com",
+  messagingSenderId: "851794594165"
+};
+firebase.initializeApp(config);
+
+const db = firebase.firestore();
+db.settings({
+  timestampsInSnapshots: true
+})
+const collection = db.collection('messages');
+
+const message = document.getElementById('message');
+const form = document.querySelector('form');
+const messages = document.getElementById('messages');
+
+collection.orderBy('created').get().then(snapshot => {
+  snapshot.forEach(doc => {
+    const li = document.createElement('li');
+    li.textContent = doc.data().message;
+    messages.appendChild(li);
+  });
+});
+
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const val = message.value.trim();
+  if (val === ""){
+    return;
+  }
+
+  const li = document.createElement('li');
+  li.textContent = val;
+  messages.appendChild(li);
+
+  message.value = '';
+  message.focus();
+
+  collection.add({
+    message: val,
+    created: firebase.firestore.FieldValue.serverTimestamp()
+  })
+  .then(doc =>{
+    console.log(`${doc.id} added!`);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+});
+
+message.focus();
+
