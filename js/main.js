eeec2697e2ccfbe14552895a9a789192e1e1d615
atsushi <peace96beats@gmail.com>
2018-07-08 00:11:38 +0900

    Complete

--- js/main.js
+++ js/main.js
@@ -1,60 +1,100 @@
-'use strict'
+(() => {
+  'use strict'
 
-const config = {
-  apiKey: "AIzaSyDnPsMPeOmNGQkWfR3rg4homLR9IUtmMZE",
-  authDomain: "myfirebasechatapp-8a7de.firebaseapp.com",
-  databaseURL: "https://myfirebasechatapp-8a7de.firebaseio.com",
-  projectId: "myfirebasechatapp-8a7de",
-  storageBucket: "myfirebasechatapp-8a7de.appspot.com",
-  messagingSenderId: "851794594165"
-};
-firebase.initializeApp(config);
+  const config = {
+    apiKey: "AIzaSyDnPsMPeOmNGQkWfR3rg4homLR9IUtmMZE",
+    authDomain: "myfirebasechatapp-8a7de.firebaseapp.com",
+    databaseURL: "https://myfirebasechatapp-8a7de.firebaseio.com",
+    projectId: "myfirebasechatapp-8a7de",
+    storageBucket: "myfirebasechatapp-8a7de.appspot.com",
+    messagingSenderId: "851794594165"
+  };
+  firebase.initializeApp(config);
 
-const db = firebase.firestore();
-db.settings({
-  timestampsInSnapshots: true
-});
-const collection = db.collection('messages');
+  const db = firebase.firestore();
+  db.settings({
+    timestampsInSnapshots: true
+  });
+  const collection = db.collection('messages');
 
-const auth = firebase.auth();
+  const auth = firebase.auth();
+  let me = null;
 
-const message = document.getElementById('message');
-const form = document.querySelector('form');
-const messages = document.getElementById('messages');
+  const message = document.getElementById('message');
+  const form = document.querySelector('form');
+  const messages = document.getElementById('messages');
+  const login = document.getElementById('login');
+  const logout = document.getElementById('logout');
 
-collection.orderBy('created').onSnapshot(snapshot => {
-  snapshot.docChanges().forEach(change => {
-    if (change.type === 'added') {
-      const li = document.createElement('li');
-      li.textContent = change.doc.data().message;
-      messages.appendChild(li);
+
+
+  login.addEventListener('click', () => {
+    auth.signInAnonymously();
+  });
+
+  logout.addEventListener('click', () => {
+    auth.signOut();
+  });
+
+  auth.onAuthStateChanged(user => {
+    if(user){
+      me = user;
+
+      while (messages.firstChild) {
+        messages.removeChild(messages.firstChild);
+      }
+
+      collection.orderBy('created').onSnapshot(snapshot => {
+        snapshot.docChanges().forEach(change => {
+          if (change.type === 'added') {
+            const li = document.createElement('li');
+            const d = change.doc.data();
+            li.textContent = d.uid.substr(0,8)+':'+d.message;
+            messages.appendChild(li);
+          }
+        });
+      }, error => {});
+
+      console.log(`Logged in as: ${user.uid}`);
+      login.classList.add('hidden');
+      [logout, form, messages].forEach(el => {
+        el.classList.remove('hidden');
+      });
+      message.focus();
+      return;
     }
+    me = null;
+    console.log('Nobody is logged in');
+    login.classList.remove('hidden');
+    [logout, form, messages].forEach(el => {
+      el.classList.add('hidden');
+    });
+
   });
-});
 
 
-form.addEventListener('submit', e => {
-  e.preventDefault();
+  form.addEventListener('submit', e => {
+    e.preventDefault();
 
-  const val = message.value.trim();
-  if (val === "") {
-    return;
-  }
+    const val = message.value.trim();
+    if (val === "") {
+      return;
+    }
 
-  message.value = '';
-  message.focus();
+    message.value = '';
+    message.focus();
 
-  collection.add({
-    message: val,
-    created: firebase.firestore.FieldValue.serverTimestamp()
-  })
-  .then(doc => {
-    console.log(`${doc.id} added!`);
-  })
-  .catch(error => {
-    console.log(error);
+    collection.add({
+      message: val,
+      created: firebase.firestore.FieldValue.serverTimestamp(),
+      uid: me ? me.uid : 'nobody'
+    })
+    .then(doc => {
+      console.log(`${doc.id} added!`);
+    })
+    .catch(error => {
+      console.log('document add error!');    
+      console.log(error);
+    });
   });
-});
-
-message.focus();
-
+})();
